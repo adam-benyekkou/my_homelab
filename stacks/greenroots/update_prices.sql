a6414dbd4e8533fad6f98995a55759bebe74a76e
Cavy Dev <cavy.protocol.dev@proton.me>
2026-02-17 15:54:23 +0000

    Added Sible Ansible Orchestrator to the stack

---
+++ stacks/greenroots/update_prices.sql
@@ -0,0 +1,19 @@
+UPDATE tree SET price_id = 'price_1SugPMFE6fL5cookFpV4m79S' WHERE name = 'Oak';
+UPDATE tree SET price_id = 'price_1SugPNFE6fL5cookhucECwmO' WHERE name = 'Baobab';
+UPDATE tree SET price_id = 'price_1SugPNFE6fL5cookMZSpWoSP' WHERE name = 'Eucalyptus';
+UPDATE tree SET price_id = 'price_1SugPOFE6fL5cookiONPRWrN' WHERE name = 'Rosewood';
+UPDATE tree SET price_id = 'price_1SugPOFE6fL5cooky4Qpatyf' WHERE name = 'Dipterocarpus';
+UPDATE tree SET price_id = 'price_1SugPPFE6fL5cookjyL3YFOa' WHERE name = 'Burmese Teak';
+UPDATE tree SET price_id = 'price_1SugPPFE6fL5cooknn6odHzW' WHERE name = 'Philippine Narra';
+UPDATE tree SET price_id = 'price_1SugPQFE6fL5cookSQFgfwmE' WHERE name = 'Brazilian Mahogany';
+UPDATE tree SET price_id = 'price_1SugPQFE6fL5cookxo0rZnNW' WHERE name = 'Cecropia';
+UPDATE tree SET price_id = 'price_1SugPRFE6fL5cookYHCjk0pZ' WHERE name = 'Pau-Brasil';
+UPDATE tree SET price_id = 'price_1SugPRFE6fL5cookxbQjXLVG' WHERE name = 'Black Spruce';
+UPDATE tree SET price_id = 'price_1SugPSFE6fL5cookOGWKIAxU' WHERE name = 'Trembling Aspen';
+UPDATE tree SET price_id = 'price_1SugPSFE6fL5cookwsf6fP5V' WHERE name = 'Red Gum Eucalyptus';
+UPDATE tree SET price_id = 'price_1SugPTFE6fL5cookzbjzIh2b' WHERE name = 'Giant Banksia';
+UPDATE tree SET price_id = 'price_1SugPTFE6fL5cookoJD8DUZI' WHERE name = 'Romanian Oak';
+UPDATE tree SET price_id = 'price_1SugPUFE6fL5cookO6QKaA7u' WHERE name = 'Cork Oak';
+UPDATE tree SET price_id = 'price_1SugPUFE6fL5cookrJMvke2P' WHERE name = 'Balanites';
+UPDATE tree SET price_id = 'price_1SugPVFE6fL5cookVx6EV0eh' WHERE name = 'Prosopis';
+
